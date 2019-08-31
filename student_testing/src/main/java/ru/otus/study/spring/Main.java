ac82118f8d1cd9c4c01a1e0f2c78e03ca60376af
cheester <cheester.x3@gmail.com>
2019-08-31 21:05:55 +0300

    AppRunner service added. Wrapped service from Main with AppRunner

--- student_testing/src/main/java/ru/otus/study/spring/Main.java
+++ student_testing/src/main/java/ru/otus/study/spring/Main.java
@@ -1,23 +1,13 @@
 package ru.otus.study.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.otus.study.spring.domain.StudentAnswer;
-import ru.otus.study.spring.domain.StudentNameInfo;
-import ru.otus.study.spring.service.StudentInfoAskService;
-import ru.otus.study.spring.service.TaskCheckService;
-import ru.otus.study.spring.service.TestingService;
+import ru.otus.study.spring.service.AppRunner;
 
 public class Main {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("/context.xml");
-        TestingService testService = context.getBean(TestingService.class);
-        TaskCheckService checkService = context.getBean(TaskCheckService.class);
-        StudentInfoAskService studentInfoAskService=context.getBean(StudentInfoAskService.class);
-        StudentNameInfo studentNameInfo=studentInfoAskService.getStudentInfo();
-        if(studentNameInfo!=null) {
-            Iterable<StudentAnswer> answers = testService.processTestingAndGetResults();
-            checkService.checkAnswersAndShowResults(answers, studentNameInfo);
-        }
+        AppRunner testService = context.getBean(AppRunner.class);
+        testService.run();
     }
 }
---
+++ student_testing/src/main/java/ru/otus/study/spring/service/AppRunnerImpl.java
@@ -0,0 +1,27 @@
+package ru.otus.study.spring.service;
+
+import ru.otus.study.spring.domain.StudentAnswer;
+import ru.otus.study.spring.domain.StudentNameInfo;
+
+import java.util.List;
+
+public class AppRunnerImpl implements AppRunner {
+    private final TestingService testService;
+    private final TaskCheckService checkService;
+    private final StudentInfoAskService studentInfoAskService;
+
+    public AppRunnerImpl(TestingService testService, TaskCheckService checkService, StudentInfoAskService studentInfoAskService) {
+        this.testService = testService;
+        this.checkService = checkService;
+        this.studentInfoAskService = studentInfoAskService;
+    }
+
+    @Override
+    public void run() {
+        StudentNameInfo studentNameInfo=studentInfoAskService.getStudentInfo();
+        if(studentNameInfo!=null) {
+            List<StudentAnswer> answers = testService.processTestingAndGetReadOnlyResults();
+            checkService.checkAnswersAndShowResults(answers, studentNameInfo);
+        }
+    }
+}
