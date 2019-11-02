5ee606094f4f4dd1535794e518d649bd65c91d7b
cheester <cheester.x3@gmail.com>
2019-11-02 13:56:16 +0300

    + vue front

---
+++ library-web-rest/library-vue/src/components/Api.vue
@@ -0,0 +1,27 @@
+<script>
+    export default {
+        name: "Api",
+        getAllBooks: () => fetch('/api/books').then(response => response.json()),
+        deleteBook: (id) => fetch('/api/books/' + id, {method: 'DELETE'}),
+        createBook: (book) => fetch('/api/books', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(book)
+        }),
+        saveBook: (book) => fetch('/api/books/' + book.id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(book)
+        }),
+        getAllGenres: () => fetch('/api/genres').then(response => response.json()),
+        getAllAuthors: () => fetch('/api/authors').then(response => response.json())
+    }
+</script>
+
+<style scoped>
+
+</style>
